d23d996753e4efed5ee063939499172ddbe6aa7c
Muhammad Sarim Khan Bangash <75114586+m-sarim-khan-bangash@users.noreply.github.com>
2024-10-03 01:05:55 +0500

    Update store.jsx

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlicer";
 import productReducer from "./productSlicer";
+import { productsApi } from "../features/apiSlice";
 
 const store = configureStore({
   reducer: {
     todo: todoReducer,
     product: productReducer,
+    [productsApi.reducerPath]: productsApi.reducer,
   },
+
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(productsApi.middleware),
 });
 
 export default store;

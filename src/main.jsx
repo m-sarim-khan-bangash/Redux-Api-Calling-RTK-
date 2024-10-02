10aec998ec7f9f8952c1545852a2d9bf2cc0c979
Muhammad Sarim Khan Bangash <75114586+m-sarim-khan-bangash@users.noreply.github.com>
2024-10-03 01:05:27 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App.jsx";
 import "./index.css";
-import { Provider } from "react-redux";
 import store from "./redux/store.jsx";
-import { ApiProvider } from "@reduxjs/toolkit/query/react";
-import { productsApi } from "./features/apiSlice.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <Provider store={store}> */}
-      <ApiProvider api={productsApi}>
-        <App />
-      </ApiProvider>
-    {/* </Provider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );

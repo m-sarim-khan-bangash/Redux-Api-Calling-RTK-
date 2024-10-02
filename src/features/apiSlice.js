240f031c96fab910d051e2fb3c80bebf103fdc90
Muhammad Sarim Khan Bangash <75114586+m-sarim-khan-bangash@users.noreply.github.com>
2024-10-03 01:05:00 +0500

    Update apiSlice.js

--- src/features/apiSlice.js
+++ src/features/apiSlice.js
@@ -3,14 +3,51 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({ baseUrl: "https://dummyjson.com" }),
+  tagTypes: ["Product"],
+
   endpoints: (builder) => ({
     getAllProducts: builder.query({
       query: () => "/products",
+      providesTags: ["Product"],
     }),
+
     getProduct: builder.query({
       query: (product) => `products/search?q=${product}`,
+      providesTags: ["Product"],
+    }),
+
+    newProduct: builder.mutation({
+      query: (newProduct) => ({
+        url: "/products",
+        method: "POST",
+        body: newProduct,
+      }),
+      invalidatesTags: ["Product"],
+    }),
+
+    deleteProduct: builder.mutation({
+      query: (id) => ({
+        url: `/products/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Product"],
+    }),
+
+    updateProduct: builder.mutation({
+      query: ({ id, ...patch }) => ({
+        url: `/products/${id}`,
+        method: "PATCH",
+        body: patch,
+      }),
+      invalidatesTags: ["Product"],
     }),
   }),
 });
 
-export const { useGetAllProductsQuery, useGetProductQuery } = productsApi;
+export const {
+  useGetAllProductsQuery,
+  useGetProductQuery,
+  useNewProductMutation,
+  useDeleteProductMutation,
+  useUpdateProductMutation,
+} = productsApi;
